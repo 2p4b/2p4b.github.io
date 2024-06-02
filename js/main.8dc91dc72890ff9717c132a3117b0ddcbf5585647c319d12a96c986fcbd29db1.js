47fca4aa3ca345f62e052555db4f8514420b1a88
2p4b <contact@2p4b.xyz>
2024-06-01 18:10:49 -0600

    site

---
+++ js/main.8dc91dc72890ff9717c132a3117b0ddcbf5585647c319d12a96c986fcbd29db1.js
@@ -0,0 +1,1 @@
+(()=>{console.log("Hello I'm 2p4b email me at 2p4b@protonmail.me for vulnerability needs.");})();
---
+++ js/main.js
@@ -0,0 +1,4 @@
+(() => {
+  // <stdin>
+  console.log("Hello I'm 2p4b email me at 2p4b@protonmail.me for vulnerability needs.");
+})();
